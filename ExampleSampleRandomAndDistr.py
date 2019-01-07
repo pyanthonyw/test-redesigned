f8787757e64547b1624da0d463dfcd5b9b4271f2
anthony.woolcock <pyanthonyw@gmail.com>
2019-01-07 14:35:34 +0000

    QUick example committed

---
+++ ExampleSampleRandomAndDistr.py
@@ -0,0 +1,32 @@
+
+
+#########################
+# Short code to select a speaker at random
+# date: 7th Jan 2019
+# Developer: AW
+# Requirements:
+import numpy as np
+import pandas as pd
+# python3
+# numpy
+# names of names_speakers - hard coded
+#########################
+names_speakers = { 1:'julien', 2:'stephen', 3:'anthony'} #https://docs.python.org/3.6/tutorial/datastructures.html
+x = np.random.rand()
+print(int( np.ceil(3*x) ))
+print(names_speakers[ int( np.ceil(3*x) ) ] )
+#########################
+# Demonstrate it working
+for j in range(5):
+    x = np.random.rand()
+    print(int( np.ceil(3*x) ))
+    print(names_speakers[ int( np.ceil(3*x) ) ] )
+#########################
+# Demonstrate something else
+N = 20*1000
+# https://en.wikipedia.org/wiki/Gamma_distribution
+x2 = np.random.gamma(2,10,N)
+df = pd.DataFrame(x2,columns=['Distr'])#,index=data[:,0]),
+print(df.head())
+print(df.Distr.hist(bins=int(N/1000))) # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.hist.html
+#########################
