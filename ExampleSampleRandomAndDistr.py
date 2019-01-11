2e0343166d7b2a7bb8fa23f1680d0b3436cca798
pyanthonyw <pyanthonyw@gmail.com>
2019-01-11 14:59:31 +0000

    Testing bin size effect on plots. Not a critical change - but interesting to see the effect. Suggested other testing: repeat using ggplot in R.

--- ExampleSampleRandomAndDistr.py
+++ ExampleSampleRandomAndDistr.py
@@ -11,7 +11,8 @@ import pandas as pd
 # numpy
 # names of names_speakers - hard coded
 #########################
-names_speakers = { 1:'j', 2:'s', 3:'a',4:'c'} #https://docs.python.org/3.6/tutorial/datastructures.html
+names_speakers = { 1:'j', 2:'s', 3:'a'} #https://docs.python.org/3.6/tutorial/datastructures.html
+np.random.seed(62)
 x = np.random.rand()
 print(int( np.ceil(3*x) ))
 print(names_speakers[ int( np.ceil(3*x) ) ] )
@@ -21,12 +22,21 @@ for j in range(5):
     x = np.random.rand()
     print(int( np.ceil(3*x) ))
     print(names_speakers[ int( np.ceil(3*x) ) ] )
+
+
+
+#X = np.random.beta(2,10)
+
 #########################
-# Demonstrate something else
+# Demonstrate something else New
 N = 20*1000
 # https://en.wikipedia.org/wiki/Gamma_distribution
 x2 = np.random.gamma(2,10,N)
 df = pd.DataFrame(x2,columns=['Distr'])#,index=data[:,0]),
 print(df.head())
 print(df.Distr.hist(bins=int(N/1000))) # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.hist.html
+print(df.Distr.hist(bins=int(N/100))) # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.hist.html
+print(df.Distr.hist(bins=int(N/10))) # https://pandas.pydata.org/pandas-docs/stable/generated/pandas.DataFrame.hist.html
+#########################
+
 #########################
